c38cfd6939281f4f048cc8cf52a42e045ff57423
Jeremy3345321 <164300914+Jeremy3345321@users.noreply.github.com>
2025-10-17 18:07:06 +0800

    added User usage example

--- scripts/UserChat.js
+++ scripts/UserChat.js
@@ -29,3 +29,27 @@ class UserWithChat extends User {
         await Database.markConversationAsRead(this.userId, otherUserId);
     }
 }
+
+
+// USAGE EXAMPLE
+
+/*
+// Example: Sending a message
+const user1 = await Database.getUserById(1);
+const user2 = await Database.getUserById(2);
+
+const message = new Message(null, user1.getUserId(), user2.getUserId(), "Hi! Is this item still available?");
+await Database.addMessage(message);
+
+// Get conversation between two users
+const conversation = await Database.getConversation(user1.getUserId(), user2.getUserId());
+
+// Get unread messages for a user
+const unreadMessages = await Database.getUnreadMessages(user2.getUserId());
+
+// Mark message as read
+await Database.markMessageAsRead(message.getMessageId());
+
+// Get all conversations for a user
+const conversations = await Database.getUserConversations(user1.getUserId());
+*/
